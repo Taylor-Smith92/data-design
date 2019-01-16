df505ba3c3d75b2843e3b70c29eaf830fe82d5f8
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-16 13:41:00 -0700

    added anchor links to index, added info for persona description, still needs completed frustrations

--- epic/brockLee.php
+++ epic/brockLee.php
@@ -4,8 +4,39 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
+
 		<h1>Data Design Project</h1>
-		<h2>User Persona</h2>
+		<h2>User Persona:</h2>
+
+		<ul>
+			<li>Name: Brock Lee</li>
+			<li>Age: 19</li>
+			<li>Gender: Male</li>
+			<li>Occupation: College Student and Part-time FroYo Artisan</li>
+			<li>Technology: Phone: Iphone 8; Laptop: MacBook; OS: High Sierra</li>
+			<li>Attitude:
+				<ul>
+					<li>Brock is laid back. He likes to toss a frisbee around the quad before class.</li>
+					<li>Brock likes to show off his talents and get as much attention as possible.</li>
+					<li>Brock regularly listens to many different genres of music.</li>
+				</ul>
+			</li>
+			<li>Needs:
+				<ul>
+					<li>A resource that can teach him the basics through advanced material.</li>
+					<li>A way to access that resource any time and at any location.</li>
+					<li>Needs to have a large selection of songs to learn that includes newly released material.</li>
+				</ul>
+			</li>
+			<li>Frustrations:
+				<ul>
+					<li>He is not good at guitar and is embarrassed to take lessons in public.</li>
+					<li>He has a dorm-mate who is always around and that makes Brock embarrassed to practice.</li>
+					<li></li>
+				</ul>
+			</li>
+		</ul>
+		<img src="brockLee.jpg" alt="Brock Lee playing guitar" />
 	</body>
 
 </html>
--- epic/index.php
+++ epic/index.php
@@ -1,7 +1,5 @@
-<?php
-
-
-
-
-
-?>
+<html>
+	<a href="brockLee.php">Persona</a> <br>
+	<a href="use-case.php">Use Case</a> <br>
+	<a href="conceptual-model.php">Conceptual Model</a>
+</html>
