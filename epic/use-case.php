c34d8344fb55df1a4b4862c7dd8eecc675c124b1
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-16 15:57:54 -0700

    second draft of use case

--- epic/use-case.php
+++ epic/use-case.php
@@ -7,24 +7,25 @@
 	<body>
 		<h1>Use Case</h1>
 		<ul>
-			<li>Title: Brock Learns a Song on Ultimate-Guitar</li>
-			<li>Description: Brock wants to learn the chord progression to a popular song posted on Ultimate-Guitar and have a quick way to access it.</li>
-			<li>Name of user & their role: Brock Lee; musician who accesses Ultimate-Guitar regularly.</li>
+			<li>Title: Brock Learns a Song on Ultimate-Guitar.com</li>
+			<li>Description: A user wants to learn the chord progression to a popular song posted on Ultimate-Guitar and have a quick way to access it.</li>
+			<li>Name of user & their role: Brock Lee; musician who recently created an Ultimate-Guitar account.</li>
 			<li>Usage Pre-Conditions: Must have an internet capable device. Must have an account and be signed in.</li>
-			<li>Usage Post-Conditions: Ultimate-Guitar saves page to favorites/My Tabs</li>
+			<li>Usage Post-Conditions: Ultimate-Guitar saves favorite posting to user's "My Tabs" folder.</li>
 			<li>Interaction Flow:
 				<ul>
 					<li>Brock enters ultimate-guitar.com into his web browser.</li>
 					<li>Server returns the main page for the Ultimate Guitar site. Brock is already signed in.</li>
-					<li>Brock sees that his My Tabs folder is empty</li>
-					<li>Brock selects the search field at the top of that page and begins typing "Bohemian Rhapsody". <br>
-						A drop down window attempts to autofill the search terms and Brock selects the term he wants to search for.</li>
+					<li>Brock sees that the counter for his "My Tabs" folder is 0.</li>
+					<li>Brock selects the search field at the top of that page and begins typing "Bohemian Rhapsody".</li>
+					<li>A drop down window attempts to autofill the search terms.</li>
+					<li>Brock selects the song title he wants to search for from the autofill list.</li>
 					<li>Server returns the results of that search.</li>
-					<li>Brock reads through the results of user submitted files and clicks the link for the one with the best rating.</li>
-					<li>The server returns the unique song page.</li>
-					<li>Brock notices the heart in the "Favorite" button is not red so he clicks it.</li>
-					<li>Server completes the request to add this song page to user's "My Tabs" folder.</li>
-					<li>Brock can see that heart inf the "Favorite" button is now red and that one tab has been added to the "My Tabs" counter.</li>
+					<li>Brock reads through the results of user submitted postings and clicks the link for the one with the best rating.</li>
+					<li>The server returns the page for the chosen posting.</li>
+					<li>Brock begins looking over the page and notices that the heart in the "Favorite" button is not red, so he clicks it.</li>
+					<li>Server completes the request to add this posting to user's "My Tabs" folder.</li>
+					<li>Brock can see that heart in the "Favorite" button is now red and that the "My Tabs" counter has increased by one.</li>
 				</ul>
 			</li>
 			<li>Frequency of Use: Everyday</li>
