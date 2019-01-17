9f22aeced0b3a0b511815975cacc58196879c7b1
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-17 15:43:25 -0700

    edited text for flow and accuracy

--- epic/use-case.php
+++ epic/use-case.php
@@ -6,16 +6,16 @@
 	</head>
 	<body>
 		<h1>Use Case</h1>
-		<p><strong>Title:</strong> Brock Favorites a Song on Ultimate-Guitar.com</p>
+		<p><strong>Title:</strong> Brock Favorites and Rates a Song on Ultimate-Guitar.com</p>
 		<p><strong>Description:</strong> A user wants to add the posting for a popular song to their "My Tabs" list on Ultimate-Guitar.</p>
 		<p><strong>Name of user & their role:</strong> Brock Lee; musician who recently created an Ultimate-Guitar account.</p>
 		<p><strong>Usage Pre-Conditions:</strong> Must have an internet capable device. Must have an account and be signed in.</p>
-		<p><strong>Post-Conditions:</strong> Ultimate-Guitar saves favorite posting to user's "My Tabs" folder.</p>
+		<p><strong>Post-Conditions:</strong> Ultimate-Guitar saves the post to Brocks's "My Tabs" list and saves his rating with the post.</p>
 		<p><strong>Interaction Flow:</strong></p>
 		<ul>
 			<li>Brock enters ultimate-guitar.com into his web browser.</li>
 			<li>Server returns the main page for the Ultimate Guitar site. Brock is already signed in.</li>
-			<li>Brock sees that the counter for his "My Tabs" folder is 0.</li>
+			<li>Brock sees that the counter for his "My Tabs" list is at 0.</li>
 			<li>Brock selects the search field at the top of that page and begins typing "Bohemian Rhapsody".</li>
 			<li>A drop down window attempts to autofill the search terms.</li>
 			<li>Brock selects the song title he wants to search for from the autofill list.</li>
@@ -24,9 +24,9 @@
 			<li>Server returns the page for the chosen posting.</li>
 			<li>Brock begins looking over the page and notices that the heart in the "Favorite" button is not red, so he clicks it.</li>
 			<li>Server completes the request to add this posting to Brock's "My Tabs" folder.</li>
-			<li>Brock can see that heart in the "Favorite" button is now red and that the "My Tabs" counter has increased by one.</li>
+			<li>Brock can see that heart in the "Favorite" button is now red and that the "My Tabs" list counter has increased by one.</li>
 			<li>At the end of the web page Brock sees that rating form on this post is also not filled in so he clicks the star representing the highest rating.</li>
-			<li>Server completes the request to add Brock's rating to the posts' average.</li>
+			<li>Server completes the request to add Brock's rating to the post and updates the average.</li>
 			<li>Brock can see that the stars in the rating form are now filled red to match his rating value.</li>
 
 		</ul>
