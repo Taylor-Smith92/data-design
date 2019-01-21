4025f98791361ce3eae94b5e9df8326edb8f89d9
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-21 13:49:24 -0700

    fixed syntax errors and set up tables in mysql

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -14,43 +14,36 @@ CREATE TABLE `user` (
 	UNIQUE(userEmail),
 	PRIMARY KEY(userId)
 );
-
-/**
 CREATE TABLE post (
 	postId BINARY(16) NOT NULL,
 	postUserId BINARY(16) NOT NULL,
 	postContent TEXT(5000) NOT NULL,
 	postDate DATETIME(6) NOT NULL,
-	postDescription TINYTEXT(50) NOT NULL,
+	postDescription TEXT(50) NOT NULL,
 	postType CHAR(5) NOT NULL,
 	postUrl VARCHAR(128) NOT NULL,
 	INDEX(postUserId),
 	FOREIGN KEY(postUserId) REFERENCES `user`(userId),
 	PRIMARY KEY(postId)
 );
-**/
-
-/**
 CREATE TABLE rating (
 	ratingPostId BINARY(16) NOT NULL,
 	ratingUserId BINARY(16) NOT NULL,
 	ratingDate DATETIME(6) NOT NULL,
-	ratingValue TINYINT(UNSIGNED) NOT NULL,
+	ratingValue TINYINT(1) NOT NULL,
 	INDEX(ratingPostId),
 	INDEX(ratingUserId),
 	FOREIGN KEY(ratingPostId) REFERENCES post(postId),
 	FOREIGN KEY(ratingUserId) REFERENCES `user`(userId),
 	PRIMARY KEY(ratingPostId, ratingUserId)
-**/
-
-/**
+);
 CREATE TABLE favorite (
 	favoritePostId BINARY(16) NOT NULL,
 	favoriteUserId BINARY(16) NOT NULL,
 	favoriteDate DATETIME(6) NOT NULL,
 	INDEX(favoritePostId),
 	INDEX(favoriteUserId),
-	FOREIGN KEY(favoritePostId) REFERENCES post(postId)
+	FOREIGN KEY(favoritePostId) REFERENCES post(postId),
 	FOREIGN KEY(favoriteUserId) REFERENCES `user`(userId),
 	PRIMARY KEY(favoritePostId, favoriteUserId)
-**/
+);
